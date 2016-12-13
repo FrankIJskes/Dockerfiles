4eae3e92a469db9ffaaa12c5ecf93019f780312b
frank <frank.ijskes@gmail.com>
2016-12-13 23:16:28 +0100

    Added shell scripts

---
+++ pycharm/pycharm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xhost +local:docker
+docker run -it -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /home/frank/Dropbox/PycharmProjects:/root/PycharmProjects -v ~/.PyCharm40:/root/.PyCharm40 -e DISPLAY=$DISPLAY --rm frank/pycharm
+
