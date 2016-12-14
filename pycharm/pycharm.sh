98db3182eea220226b4c35bb314aca9104e326ae
frank <frank.ijskes@gmail.com>
2016-12-14 21:31:10 +0100

    Pycharm 2016.3.1 and _all_ python modules and fixes to the startup script.

--- pycharm/pycharm.sh
+++ pycharm/pycharm.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 xhost +local:docker
-docker run -it -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /home/frank/Dropbox/PycharmProjects:/root/PycharmProjects -v ~/.PyCharm40:/root/.PyCharm40 -e DISPLAY=$DISPLAY --rm frank/pycharm
+
+# .PyCharmCE2016.3 is the default directory to store the settings for pycharm
+# It uses the X socket
+# and a redirect for the projects.
+
+docker run -it \
+  -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+  -v /home/frank/Dropbox/PycharmProjects:/root/PycharmProjects \
+  -v ~/.PyCharmCE2016.3:/root/.PyCharmCE2016.3 \
+  -e DISPLAY=$DISPLAY \
+  --rm  \
+  frank/pycharm
 
