3937c8fee2b60e1cb899c3681e131468b1ec1397
FrankIJskes <frank.ijskes@gmail.com>
2017-01-02 14:44:24 +0100

    Update pycharm.sh

--- pycharm/pycharm.sh
+++ pycharm/pycharm.sh
@@ -12,11 +12,13 @@ xhost +local:docker
 # .PyCharmCE2016.3 is the default directory to store the settings for pycharm
 # It uses the X socket
 # and a redirect for the projects.
+# and allow for the default port when developing with web.py
 
 docker run -it \
   -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
   -v /home/frank/Dropbox/PycharmProjects:/root/PycharmProjects \
   -v ~/.PyCharmCE2016.3:/root/.PyCharmCE2016.3 \
+  -p 8080:8080
   -e DISPLAY=$DISPLAY \
   --rm  \
   frank/pycharm
