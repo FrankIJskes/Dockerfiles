1a64f2ab61b139c05a3f020e00cfc4d5f64f57eb
FrankIJskes <frank.ijskes@gmail.com>
2017-06-21 20:31:33 +0200

    fixed small issues

--- pycharm/pycharm.sh
+++ pycharm/pycharm.sh
@@ -5,11 +5,10 @@
 echo -en "\033]0;frankij/PyCharm CE\a"
 
 # minimize the terminal
-xdotool windowminimize $(xdotool getactivewindow)
+#xdotool windowminimize $(xdotool getactivewindow)
 
 xhost +local:docker
 
-# .PyCharmCE2016.3 is the default directory to store the settings for pycharm
 # It uses the X socket
 # and a redirect for the projects.
 # and allow for the default port when developing with web.py
@@ -17,7 +16,7 @@ xhost +local:docker
 nohup docker run -it \
   -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
   -v /home/frank/Dropbox/PycharmProjects:/root/PycharmProjects \
-  -v ~/.PyCharmCE2016.3:/root/.PyCharmCE2016.3 \
+  -v ~/.PyCharmCE2017.1:/root/.PyCharmCE2017.1 \
   -p 8080:8080 \
   -e DISPLAY=$DISPLAY \
   --rm  \
