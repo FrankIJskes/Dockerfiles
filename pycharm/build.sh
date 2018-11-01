d42ce6dcd58f6c7fd5c24a8929824736b0cf33a2
ijskesf <frank.ijskes@rws.nl>
2018-11-01 14:14:10 +0100

    pymongo added

--- pycharm/build.sh
+++ pycharm/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker build . -t frankij/pycharm
+if [ "$1" == "" ]; then
+	echo 'Geef buildnummer mee'
+	exit 1
+fi
+
+
+
+docker build . -t frankij/pycharm:$1 --build-arg PASSED_VERSION=$1 --label $1
+docker tag frankij/pycharm:$1 frankij/pycharm:latest
+
--- pycharm/pycharm.sh
+++ pycharm/pycharm.sh
@@ -16,7 +16,7 @@ xhost +local:docker
 docker run  \
   -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
   -v /home/frank/Dropbox/PycharmProjects:/root/PycharmProjects \
-  -v ~/.PyCharmCE2017:/root/.PyCharmCE2017.3 \
+  -v ~/.PyCharmCE2018:/root/.PyCharmCE2018.2 \
   -p 8080:8080 \
   -e DISPLAY=$DISPLAY \
   --rm  \
