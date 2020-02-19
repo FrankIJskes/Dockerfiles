f5da1a4a9949366c52a5f69a75b1ca04d43dc139
ijskesf <frank.ijskes@rws.nl>
2020-02-19 11:37:51 +0100

    Cleanup

--- pycharm/build.sh
+++ pycharm/build.sh
@@ -5,8 +5,8 @@ if [ "$1" == "" ]; then
 	exit 1
 fi
 
+short_version=$( echo $1 |  awk -F. '{ print $1"."$2 }' )
 
-
-docker build . -t frankij/pycharm:$1 --build-arg PASSED_VERSION=$1 --label $1
+docker build . -t frankij/pycharm:$1 --build-arg PASSED_VERSION=$1 --build-arg SHORT_VERSION=$short_version --label $1
 docker tag frankij/pycharm:$1 frankij/pycharm:latest
-
+docker push frankij/pycharm:latest
